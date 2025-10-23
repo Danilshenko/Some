08a16876d4f5a88ba253dad850e25e7401ebe20c
Danil <pidog@example.com>
2025-10-23 13:32:36 +0300

    Add CTA component to version control

--- some-app/src/components/SpecialOffersHeader/SpecialOffersHeader_temp.jsx
+++
@@ -1,9 +0,0 @@
-function SpecialOffersHeader({ title, description }) {
-  return (
-    <div className="special-offers__name">
-      <h2 className="name-top__text">{title}</h2>
-      <h3 className="name-bottom__text">{description}</h3>
-    </div>
-  );
-}
-export default SpecialOffersHeader;
