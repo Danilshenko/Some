85e40577294e563cf80a2c032d3714f3141872fc
Danil <pidog@example.com>
2025-10-23 13:03:28 +0300

    Temporary rename files to fix case

---
+++ some-app/src/components/CTA/Cta_temp.jsx
@@ -0,0 +1,21 @@
+import "../CTA/cta.css";
+import Button from "../elements/Button/Button";
+
+function CTA() {
+  return (
+    <section className="cta-block">
+      <img
+        className="cta-img"
+        src="./img/bg/imgfootre-ezgif.com-jpg-to-webp-converter.webp"
+        alt="back CTA"
+      />
+      <div className="cta-sing">
+        <h2 className="cta-top__text">Create your own</h2>
+        <h3 className="cta-bottom__text">perfect interior</h3>
+        <Button>Sing up now</Button>
+      </div>
+    </section>
+  );
+}
+
+export default CTA;
